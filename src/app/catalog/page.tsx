5051400a108fabef0372948a45c97fc69eaeb41d
Melory Ayala <melory.ayala@gmail.com>
2022-07-21 16:50:04 +0200

    feat: add catalog page

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,26 @@
+import {Badge} from "@/components/ui/badge";
+import {ShapesIcon} from "lucide-react";
+import {prismaClient} from "@/lib/prisma";
+import CategoryCardItem from "@/components/customComponents/categoryCardItem";
+
+const CatalogPage = async () => {
+    const categories = await prismaClient.category.findMany({});
+
+    return (
+        <div className="flex flex-col gap-8 p-5">
+            <Badge
+                className="w-fit gap-2 text-base uppercase border-primary border-2 px-3 py-1"
+                variant="outline">
+                <ShapesIcon size={16} />
+                Catalog
+            </Badge>
+
+            <div className="grid grid-cols-2 flex-wrap gap-8">
+                {categories.map(category => (
+                    <CategoryCardItem category={category} />
+                ))}
+            </div>
+        </div>
+    );
+};
+export default CatalogPage;
