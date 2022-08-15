d90886ad89a785e553edb6bac9c86973c98a1890
Melory Ayala <melory.ayala@gmail.com>
2022-08-15 10:30:51 +0200

    fix: ensure cart products can be scrolled

--- src/components/customComponents/cart.tsx
+++ src/components/customComponents/cart.tsx
@@ -4,13 +4,15 @@ import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import CartItem from "@/components/customComponents/cartItem";
 import {Separator} from "@/components/ui/separator";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {Button} from "@/components/ui/button";
 
 const Cart = ({}) => {
   const { products,subTotal, total, totalDiscount } =
     useContext(CartContext);
 
   return (
-    <div className="flex flex-col p-5 gap-8">
+    <div className="flex h-full flex-col gap-8 p-5">
       <Badge
         className="w-fit gap-2 border-2 border-primary px-3 py-1 text-base uppercase"
         variant="outline"
@@ -19,41 +21,49 @@ const Cart = ({}) => {
         Shopping Cart
       </Badge>
 
-      <div className="flex flex-col gap-5">
+      <ScrollArea className="h-full ">
+        <div className="flex flex-col gap-5">
           {products.length > 0 ? (
-              products.map((product) => (
-                  <CartItem key={product.id} product={product} />
-              ))
-          ): (
-              <p className="text-center opacity-60">You don't have products yet.</p>
+            products.map((product) => (
+              <CartItem key={product.id} product={product} />
+            ))
+          ) : (
+            <p className="text-center opacity-60">
+              You don't have products yet.
+            </p>
           )}
-      </div>
-
-        <div className="flex flex-col gap-3">
-            <Separator />
-            <div className="flex items-center justify-between text-sm font-semibold">
-                <p>Subtotal</p>
-                <p>€ {subTotal.toFixed(2)}</p>
-            </div>
-
-            <Separator />
-            <div className="flex items-center justify-between text-sm font-semibold">
-                <p>Delivery</p>
-                <p>Free</p>
-            </div>
-
-            <Separator />
-            <div className="flex items-center justify-between text-sm font-semibold">
-                <p>Discounts</p>
-                <p>- {totalDiscount.toFixed(2)}</p>
-            </div>
-
-            <Separator />
-            <div className="flex items-center justify-between text-sm font-semibold">
-                <p>Total</p>
-                <p>€ {total.toFixed(2)}</p>
-            </div>
         </div>
+      </ScrollArea>
+
+      <div className="flex flex-col gap-3">
+        <Separator />
+        <div className="flex items-center justify-between text-sm font-semibold">
+          <p>Subtotal</p>
+          <p>€ {subTotal.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+        <div className="flex items-center justify-between text-sm font-semibold">
+          <p>Delivery</p>
+          <p>Free</p>
+        </div>
+
+        <Separator />
+        <div className="flex items-center justify-between text-sm font-semibold">
+          <p>Discounts</p>
+          <p>- {totalDiscount.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+        <div className="flex items-center justify-between text-sm font-semibold">
+          <p>Total</p>
+          <p>€ {total.toFixed(2)}</p>
+        </div>
+
+          <Button className="uppercase font-bold mt-5">
+              Purchase
+          </Button>
+      </div>
     </div>
   );
 };
